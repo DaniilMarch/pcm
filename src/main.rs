8d57f92fec45241a51a70ee41119061377dcc5e2
dmarch0 <daniil2305@yandex.ru>
2022-04-04 17:12:20 +0300

    tech: test

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use byteorder::{LittleEndian, WriteBytesExt};
 use pcm::prelude::*;
 use pcm::{write_to_wav};
 
-//test
+//test3
 fn main() {
     let mut t: f64 = 0.0;
     let mut samples: Vec<f64> = vec![];
